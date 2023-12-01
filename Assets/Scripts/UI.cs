7c582807eee978ab5396bbf93c4d8f537468a9ca
James McDonagh <jwmcdona@lakeheadu.ca>
2023-11-30 20:57:18 -0500

    save

--- Assets/Scripts/UI.cs
+++ Assets/Scripts/UI.cs
@@ -5,13 +5,8 @@ using UnityEngine.UI;
 
 public class UI : MonoBehaviour
 {
-
-
-
     // Start is called before the first frame update
-    void Start()
-    {
-    }
+    void Start() {}
 
     // Update is called once per frame
     void Update()
